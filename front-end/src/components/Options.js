f316215ef494fc65068cb0114ae2760c76b7fc90
McManOfTheLand <forrestchew@gmail.com>
2021-12-28 16:41:11 -0800

    new front end kinda

---
+++ front-end/src/components/Options.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Buttons from './Buttons';
+
+const Options = () => {
+    return (
+        <div className='options'>
+            <Buttons />
+        </div>
+    )
+}
+
+export default Options
---
+++ front-end/src/utility/proposeLoanForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+const proposeLoanForm = React.createElement(
+        <form>
+          <label>Amount to borrow <br></br>
+            <input type="text" />
+          </label> <br></br>
+          <label>Interest Rate Percentage <br></br>
+            <input type="text" />
+          </label> <br></br>
+          <label>Loan Duration <br></br>
+            <input type="text" />
+          </label>
+        </form>
+)
+
+export default proposeLoanForm
