cf83218163f28bad71612028f90de091cb5152e6
McManOfTheLand <forrestchew@gmail.com>
2022-01-17 11:52:29 -0800

    Added new fractional loan browse tab

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import About from './components/About';
 import BrowseLoans from './components/Loans/BrowseLoans';
 import NavBar from './components/NavBar';
 import ProposeLoan from './components/LoanProposals/ProposeLoan';
 import UsersPositions from './components/UsersPositions/UsersPositions';
+import BrowseFractionalLoans from './components/Loans/BrowseFractionalLoans';
 
 function App() {
     return (
@@ -13,6 +13,7 @@ function App() {
       <Routes>
           <Route path='/' element={ <About /> } />
           <Route path='/BrowseLoans' element={ <BrowseLoans /> } />
+          <Route path='BrowseFractionalLoans' element={ <BrowseFractionalLoans /> } />
           <Route path='/ProposeLoan' element={ <ProposeLoan /> } />
           <Route path='/UsersPositions' element={ <UsersPositions /> } />
       </Routes>
