d22966b9c93c352ab61a31effb6b4d81d08ed99d
McManOfTheLand <forrestchew@gmail.com>
2022-01-23 14:54:28 -0800

    Updated component names to reflect changed component names

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -1,21 +1,21 @@
 import { Routes, Route } from 'react-router-dom';
 import About from './components/About';
-import BrowseLoans from './components/Loans/BrowseLoans';
+import BrowseLoans from './components/BrowseLoans/BrowseLoans';
 import NavBar from './components/NavBar';
-import ProposeLoan from './components/LoanProposals/ProposeLoan';
-import UsersPositions from './components/UsersPositions/UsersPositions';
-import BrowseFractionalLoans from './components/Loans/BrowseFractionalLoans';
+import ProposeLoan from './components/ProposeLoan';
+import UsersPositions from './components/UsersPositions';
+import BrowseFractionalLoans from './components/BrowseLoans/BrowseFractionalLoans';
 
 function App() {
     return (
     <>
       <NavBar />
       <Routes>
-          <Route path='/' element={ <About /> } />
-          <Route path='/BrowseLoans' element={ <BrowseLoans /> } />
-          <Route path='BrowseFractionalLoans' element={ <BrowseFractionalLoans /> } />
-          <Route path='/ProposeLoan' element={ <ProposeLoan /> } />
-          <Route path='/UsersPositions' element={ <UsersPositions /> } />
+          <Route path="/" element={ <About /> } />
+          <Route path="/BrowseLoans" element={ <BrowseLoans /> } />
+          <Route path="BrowseFractionalLoans" element={ <BrowseFractionalLoans /> } />
+          <Route path="/ProposeLoan" element={ <ProposeLoan /> } />
+          <Route path="/UsersPositions" element={ <UsersPositions /> } />
       </Routes>
     </>
   );
