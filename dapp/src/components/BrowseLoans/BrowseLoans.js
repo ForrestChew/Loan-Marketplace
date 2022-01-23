0806a32bcf24f6befb7b2ff952d3fd4e0bef82dc
McManOfTheLand <forrestchew@gmail.com>
2022-01-23 14:49:12 -0800

    added new component to browse loans

---
+++ dapp/src/components/BrowseLoans/BrowseLoans.js
@@ -0,0 +1,44 @@
+import { useState, useEffect, useRef } from 'react';
+import { useMoralis } from 'react-moralis';
+import Strip from '../strips/Strip';
+
+const BrowseLoans = () => {
+    const { Moralis } = useMoralis();
+    const hasFetchedData = useRef(false);
+    const [loans,setLoans] = useState([]);
+
+    useEffect(() => {
+        const getLoans = async() => {
+            if (!hasFetchedData.current) {
+                await Moralis.enableWeb3(); 
+                const query = new Moralis.Query('LoanProposals');
+                const allLoans = await query.find();
+                setLoans(allLoans);
+                hasFetchedData.current = true;
+            }
+        }
+        getLoans();
+    }, [Moralis, loans]);
+    return (
+        <>
+            {loans.map((loan) => {
+                const { id } = loan;
+                const { Amount, InterestRate, LoanDuration, Borrower, InterestRateAmount } = loan.attributes;
+                return (
+                    <div key={id}>
+                        <Strip 
+                            amount={`Asking amount in ETH: ${Amount}`} 
+                            interestRate={`Interest Rate: ${InterestRate}%`}
+                            duration={`Loan Duration: ${LoanDuration} days`}
+                            borrower={`Proposer: ${Borrower}`}
+                            interestAmount={InterestRateAmount}
+                            id={id}
+                        />
+                    </div>
+                )
+            })}
+        </>
+    )
+}
+
+export default BrowseLoans
