51fca8a922dece18b7ef1e5eb680db50c2968626
McManOfTheLand <forrestchew@gmail.com>
2022-01-30 14:30:32 -0800

    Added testing fixture for fractional loans and loan proposal deletion. Also modified comments to things clearer

--- debt_marketplace/tests/conftest.py
+++ debt_marketplace/tests/conftest.py
@@ -3,24 +3,28 @@ from brownie import Loans, accounts
 from scripts.utils import get_account
 from web3 import Web3 as w3
 
-
+# This resets the firxtures for each test
 @pytest.fixture(autouse=True)
 def isolation(fn_isolation):
     pass
 
 
+# Ganache-cli accounts[0] is defined as account.
+# This account will be used as the "borrower" throughout testing.
 @pytest.fixture
 def account():
     account = get_account()
     return account
 
 
+# Contract instance is created
 @pytest.fixture
 def deploy_contract(account):
     contract = Loans.deploy({"from": account})
     return contract
 
 
+# User makes loan proposal
 @pytest.fixture
 def propose_loans(deploy_contract, account):
     contract = deploy_contract
@@ -30,6 +34,15 @@ def propose_loans(deploy_contract, account):
     return propose_loan_tx
 
 
+@pytest.fixture()
+def delete_loan_proposal(deploy_contract, propose_loans, account):
+    borrower = account
+    contract = deploy_contract
+    propose_loans
+    contract.deleteLoanProposal({"from": borrower})
+
+
+# Sets up a loan where a user lends to an unfilled loan
 @pytest.fixture
 def lend(deploy_contract, propose_loans, account):
     borrower = account
@@ -42,25 +55,25 @@ def lend(deploy_contract, propose_loans, account):
     return lend_tx
 
 
+# Sets up a loan where the borrower paysback the lender
 @pytest.fixture()
 def payback(deploy_contract, propose_loans, account):
     borrower = account
     lender = accounts[1]
     contract = deploy_contract
     propose_loans
-    lend
     amount_borrowed = contract.activeLoans(lender, borrower)[0]
     amount_borrowed_interest = contract.activeLoans(lender, borrower)[6]
     total_debt = amount_borrowed + amount_borrowed_interest
     contract.payback(lender, {"from": borrower, "value": total_debt})
 
 
+# Sets up a loan where the lender lists a 100% of that loan to sell
 @pytest.fixture
-def list_loan(deploy_contract, propose_loans, lend, account):
+def list_loan(deploy_contract, lend, account):
     borrower = account
     lender = accounts[1]
     contract = deploy_contract
-    propose_loans
     lend
     list_loan_tx = contract.listLoan(
         borrower, w3.toWei(1, "ether"), 100, {"from": lender}
@@ -68,17 +81,64 @@ def list_loan(deploy_contract, propose_loans, lend, account):
     return list_loan_tx
 
 
+# Sets up a loan where the lender lists a portion of that loan to sell
 @pytest.fixture
-def buy_loan(deploy_contract, propose_loans, lend, list_loan, account):
+def list_fractional_loan(deploy_contract, lend, account):
+    borrower = account
+    lender = accounts[1]
+    contract = deploy_contract
+    lend
+    list_fractional_loan_tx = contract.listLoan(
+        borrower, w3.toWei(0.5, "ether"), 50, {"from": lender}
+    )
+    return list_fractional_loan_tx
+
+
+# Sets up a loan where the lender wants to sell 100% of
+@pytest.fixture
+def buy_loan(deploy_contract, list_loan, account):
     contract = deploy_contract
     borrower = account
     lender = accounts[1]
     buyer = accounts[2]
-    propose_loans
-    lend
     list_loan
     loan_price = contract.activeLoans(lender, borrower)[5]
     buy_loan_tx = contract.buyLoan(
         lender, borrower, {"from": buyer, "value": loan_price}
     )
     return buy_loan_tx
+
+
+# Simulates a loan where the lender wants to sell part of
+@pytest.fixture
+def buy_loan_fractional(deploy_contract, list_fractional_loan, account):
+    contract = deploy_contract
+    borrower = account
+    lender = accounts[1]
+    buyer = accounts[2]
+    list_fractional_loan
+    base_loan_amount = contract.activeLoans(lender, borrower)[0]
+    interest_rate_amount = contract.activeLoans(lender, borrower)[2]
+    loan_fraction_percentage = contract.activeLoans(lender, borrower)[6]
+    fractional_for_sale_price = contract.activeLoans(lender, borrower)[5]
+    # Calculates the fractional loan amount using the equation
+    total_loan_fraction_amount = (
+        (base_loan_amount + interest_rate_amount)
+        * loan_fraction_percentage
+        / (100 * 10 ** 18)
+    )
+    # Converts result of above number to wei
+    total_loan_fraction_amount_wei = w3.toWei(total_loan_fraction_amount, "ether")
+    # Finds the new base loan amount by taking the current base loan amount and subtracting #
+    # the total loan fraction amount
+    new_base_loan_amount = (
+        base_loan_amount + interest_rate_amount
+    ) - total_loan_fraction_amount_wei
+    buy_loan_fraction_tx = contract.buyLoanFraction(
+        lender,
+        borrower,
+        total_loan_fraction_amount_wei,
+        new_base_loan_amount,
+        {"from": buyer, "value": fractional_for_sale_price},
+    )
+    return buy_loan_fraction_tx
