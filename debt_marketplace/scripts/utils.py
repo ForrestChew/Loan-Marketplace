71f7596ea0c5f83ced45c1821885b0046ca6d367
McManOfTheLand <forrestchew@gmail.com>
2022-02-01 15:46:50 -0800

    Added the avax-test (Fuji) to the TESTNET_BLOCKCHAIN_ENVIROMENTS in utility script so that the contract can automatically be verified on deployment to the Fuji Testnet

--- debt_marketplace/scripts/utils.py
+++ debt_marketplace/scripts/utils.py
@@ -1,7 +1,7 @@
 from brownie import accounts, network, config
 
 LOCAL_BLOCKCHAIN_ENVIROMENTS = ["development", "ganache-cli", "mainnet-fork"]
-TESTNET_BLOCKCHAIN_ENVIROMENTS = ["rinkeby", "kovan"]
+TESTNET_BLOCKCHAIN_ENVIROMENTS = ["rinkeby", "kovan", "avax-test"]
 
 def get_account():
     if network.show_active() in TESTNET_BLOCKCHAIN_ENVIROMENTS:
