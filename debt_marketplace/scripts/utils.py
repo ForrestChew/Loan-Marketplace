cd0d5169b524b6e2f915bf6494e4f9dbfe075c42
McManOfTheLand <forrestchew@gmail.com>
2022-02-03 09:52:08 -0800

    Removed unnecessary blockchain list and added concise comments

--- debt_marketplace/scripts/utils.py
+++ debt_marketplace/scripts/utils.py
@@ -1,12 +1,11 @@
+# This script carries utility for other scripts.  
 from brownie import accounts, network, config
 
-LOCAL_BLOCKCHAIN_ENVIROMENTS = ["development", "ganache-cli", "mainnet-fork"]
-
-# Does not contain avax-test since it takes more effort to verify
-# contract than any Ethereum based network
 ETHEREUM_BLOCKCHAIN_ENVIROMENTS = ["rinkeby", "kovan"]
 
+# Gets account based on what network you are attempting to deploy smart contract to
 def get_account():
     if network.show_active() in ETHEREUM_BLOCKCHAIN_ENVIROMENTS:
         return accounts.add(config["wallets"]["from_key"])
+    # Returns the first Ganache account whe deploying to a local network
     return accounts[0]
