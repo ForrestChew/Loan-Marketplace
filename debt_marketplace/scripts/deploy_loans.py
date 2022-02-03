2b1a7f76d535594452d2c8950be77f7e970ea7cb
McManOfTheLand <forrestchew@gmail.com>
2022-02-03 09:40:39 -0800

    Specified that contracts will only programmatically verify if deployed to an Ethereum based network

--- debt_marketplace/scripts/deploy_loans.py
+++ debt_marketplace/scripts/deploy_loans.py
@@ -1,12 +1,11 @@
 from brownie import Loans, network
-from scripts.utils import TESTNET_BLOCKCHAIN_ENVIROMENTS
+from scripts.utils import ETHEREUM_BLOCKCHAIN_ENVIROMENTS
 from scripts.utils import get_account
 
-
+# Will programmatically verify contract on deployment if deployed to an Ethereum network
 publish_source = (
-    True if network.show_active() in TESTNET_BLOCKCHAIN_ENVIROMENTS else False
+    True if network.show_active() in ETHEREUM_BLOCKCHAIN_ENVIROMENTS else False
 )
-
 
 def deploy_loan_contract():
     account = get_account()
--- debt_marketplace/scripts/utils.py
+++ debt_marketplace/scripts/utils.py
@@ -4,9 +4,9 @@ LOCAL_BLOCKCHAIN_ENVIROMENTS = ["development", "ganache-cli", "mainnet-fork"]
 
 # Does not contain avax-test since it takes more effort to verify
 # contract than any Ethereum based network
-TESTNET_BLOCKCHAIN_ENVIROMENTS = ["rinkeby", "kovan"]
+ETHEREUM_BLOCKCHAIN_ENVIROMENTS = ["rinkeby", "kovan"]
 
 def get_account():
-    if network.show_active() in TESTNET_BLOCKCHAIN_ENVIROMENTS:
+    if network.show_active() in ETHEREUM_BLOCKCHAIN_ENVIROMENTS:
         return accounts.add(config["wallets"]["from_key"])
     return accounts[0]
