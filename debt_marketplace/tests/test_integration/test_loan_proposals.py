920f26de0a5b6245e4bc1c3f98e723b4ed858a8b
McManOfTheLand <forrestchew@gmail.com>
2022-02-03 10:41:56 -0800

    Cleaned up comments

--- debt_marketplace/tests/test_integration/test_loan_proposals.py
+++ debt_marketplace/tests/test_integration/test_loan_proposals.py
@@ -1,22 +1,21 @@
+# Tests the proposeLoan frunctions functionality.
+# Tests use setup fixtures that can be found in conftest.py
 import brownie
-from brownie import accounts
 from web3 import Web3
 
 # using Ganache-cli as the provider
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
 
-
+# Tests that a user can propose a loan
 def test_propose_loan(deploy_contract, propose_loans, account):
-    # Builds propose loan transaction
     contract = deploy_contract
-    # Uses fixture to propose loans
     propose_loans
-    # Confirming that struct fields are equal to the correct struct fields
+    # Confirms that the loan proposal now exists with the correct fields
     assert contract.proposedLoans(account)[0] == w3.toWei(1, "ether")
     assert contract.proposedLoans(account)[1] == 5
     assert contract.proposedLoans(account)[2] == w3.toWei(0.05, "ether")
     # Multiplies the input number of days, which in this case is 10,
-    # by 86,400 to get the number of seconds
+    # by 86,400 to get the number of seconds. The will help determine when the loan expires down the road.
     assert contract.proposedLoans(account)[3] == 10 * 86400
     assert contract.proposedLoans(account)[4] == 0
     assert contract.proposedLoans(account)[5] == 0
@@ -29,12 +28,12 @@ def test_propose_loan(deploy_contract, propose_loans, account):
     assert contract.proposedLoans(account)[10] is False
     assert contract.proposedLoans(account)[11] is False
 
-
+# Confirms that a user can only have one loan proposal
 def test_propose_loan_reverts(deploy_contract, propose_loans, account):
     contract = deploy_contract
-    # Proposes first loan
     propose_loans
-    # Attempts to propse a second loan with an already active proposal
+    # Attempts to propse a second loan. Will revert since any account can
+    # only have one proposal at any given time.
     with brownie.reverts("Proposal already exists"):
         propose_loan_tx = contract.proposeLoan(
             w3.toWei(2, "ether"),
