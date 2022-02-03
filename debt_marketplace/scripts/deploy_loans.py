a4f96242db546d522ddb2df94be7804bb682ffa6
McManOfTheLand <forrestchew@gmail.com>
2022-02-03 09:51:35 -0800

    added concise comments

--- debt_marketplace/scripts/deploy_loans.py
+++ debt_marketplace/scripts/deploy_loans.py
@@ -1,12 +1,15 @@
+# This script is responsible for deploying smart contract to the blockchain
 from brownie import Loans, network
 from scripts.utils import ETHEREUM_BLOCKCHAIN_ENVIROMENTS
 from scripts.utils import get_account
 
-# Will programmatically verify contract on deployment if deployed to an Ethereum network
+# Will programmatically verify smart contract on deployment 
+# if the contract is deployed to an Ethereum network
 publish_source = (
     True if network.show_active() in ETHEREUM_BLOCKCHAIN_ENVIROMENTS else False
 )
 
+# Deploys the smart contract from account. The get account function can be found in scripts/utils.py
 def deploy_loan_contract():
     account = get_account()
     print(account)
