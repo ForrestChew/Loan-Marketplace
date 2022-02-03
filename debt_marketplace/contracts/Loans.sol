ba44ed20d456ddd972a27cc3a0b22faf419df18b
McManOfTheLand <forrestchew@gmail.com>
2022-02-02 19:33:03 -0800

    Recommented and refactored contract

--- debt_marketplace/contracts/Loans.sol
+++ debt_marketplace/contracts/Loans.sol
@@ -2,22 +2,27 @@
 pragma solidity ^0.8.0;
 
 contract Loans {
+    // Represents a loan. The struct fields will update as the loan changes
     struct Loan {
-        uint256 amount; //0
-        uint256 interestPercentage; //1
-        uint256 interestAmount; //2
-        uint256 duration; //3
-        uint256 timestampStart; //4
-        uint256 forSalePrice; //5
-        uint256 loanFractionPercentage; //6 //Percentage split if lender decides to sell borrowers debt fractionally
-        uint256 loanFractionAmount; //7
-        address fractionalOwner; //8
-        bool isProposed; //9
-        bool isActive; //10
-        bool isForSale; //11
+        uint256 amount;
+        uint256 interestPercentage;
+        uint256 interestAmount;
+        uint256 duration;
+        // Logs the timestamp in order to determine when the loan expires.
+        uint256 timestampStart;
+        uint256 forSalePrice;
+        uint256 loanFractionPercentage;
+        uint256 loanFractionAmount;
+        address fractionalOwner;
+        bool isProposed;
+        bool isActive;
+        bool isForSale;
     }
+    // Tracks addresses that cannot participate in the marketplace
     mapping(address => bool) public isBlacklisted;
+    // Tracks who has loan proposals
     mapping(address => Loan) public proposedLoans;
+    // Tracks loans that have been filled
     mapping(address => mapping(address => Loan)) public activeLoans;
 
     event Proposal(
@@ -26,23 +31,24 @@ contract Loans {
         uint256 duration,
         address indexed proposer
     );
-
     event ProposalFilled(address indexed lender, address indexed borrower);
-    //Accounts who do not repay debts get blacklisted from using the lending platform
     event Blacklisted(address indexed indebted);
 
+    // Users can create loan proposals if their address is not blacklisted
     function proposeLoan(
         uint256 _amount,
         uint256 _interesetRatePercent,
         uint256 _duration
     ) external blackListedCheck {
         require(
-            proposedLoans[msg.sender].isProposed == false,
-            "Account already has proposed loan or has active loan"
+            !proposedLoans[msg.sender].isProposed,
+            "Proposal already exists"
         );
+        // Calculates the interest rate amount in ETH based on the interest percentage
         uint256 interestRateAmount = (_amount *
             _interesetRatePercent *
             10**18) / (100 * 10**18);
+        // Sets loan struct to reflect loan proposal
         proposedLoans[msg.sender] = Loan(
             _amount,
             _interesetRatePercent,
@@ -54,20 +60,27 @@ contract Loans {
             0,
             0,
             address(0),
-            true, //isProposed
-            false, //isActive
-            false //isForSale
+            true,
+            false,
+            false
         );
         emit Proposal(_amount, _interesetRatePercent, _duration, msg.sender);
     }
 
+    // Users can fill a loan proposal if their address is not blacklisted
     function lend(address payable _borrower) public payable blackListedCheck {
-        //make sure loan exits and not active
+        // Confirms that the current loan proposal exists
         require(
             proposedLoans[_borrower].isProposed,
             "Account has no active loan proposals"
         );
-        // assign ownership of the loan and updates loan status to being active
+        /* 
+        1) Maps the lender to the borrower.
+        2) Logs the current time stamp
+        3) Changes isProposed to false
+        4) Changes isActive to true
+        All other loan struct fields match the proposal
+        */
         activeLoans[msg.sender][_borrower] = Loan(
             proposedLoans[_borrower].amount,
             proposedLoans[_borrower].interestPercentage,
@@ -83,26 +96,28 @@ contract Loans {
             true,
             false
         );
-        //Deletes proposed loan mapping and updates it with who lent to the
-        // borrower using a nested mapping
+        // Deletes proposed loan
         delete proposedLoans[_borrower];
-        uint256 amountToLend = activeLoans[msg.sender][_borrower].amount;
-        //Transfers proposed loan amount from lender to borrower
-        (bool success, ) = _borrower.call{value: amountToLend}("");
+        // Transfers the asking amount from lender to loan proposer (borrower)
+        (bool success, ) = _borrower.call{
+            value: activeLoans[msg.sender][_borrower].amount
+        }("");
         require(success, "Transaction failed");
         emit ProposalFilled(msg.sender, _borrower);
     }
 
-    //Borrower's call this function to pay back their loan
+    // Borrowers can payback their debts
     function payback(address payable _lender) public payable {
         require(
             activeLoans[_lender][msg.sender].isActive,
             "Nonexistant loan cannot be paid back"
         );
-        /*The if block will run when loan is fractional. The amount each lender gets is
-        calculated and transfered to their account*/
-        // Caluculates total borrower debt. Base loan + interest amount
-        //Ensures the correct amount of ETH is paid back
+        /* 
+        Determines whether the loan is fractional or not
+        by checking whether the fractional address on the 
+        loan is the zero address or not. 
+        The if block will run if the loan IS fractional. 
+        */
         if (activeLoans[_lender][msg.sender].fractionalOwner != address(0)) {
             require(
                 msg.value ==
@@ -110,32 +125,49 @@ contract Loans {
                         activeLoans[_lender][msg.sender].loanFractionAmount,
                 "Amount paid back must be exact"
             );
+            // Transfers amount owed to lender from borrower
             (bool success, ) = _lender.call{
                 value: activeLoans[_lender][msg.sender].amount
             }("");
             require(success, "Transaction failed");
+            // Transfers amount owed to fractional owner from borrower
             (bool accept, ) = activeLoans[_lender][msg.sender]
                 .fractionalOwner
                 .call{
                 value: activeLoans[_lender][msg.sender].loanFractionAmount
             }("");
             require(accept, "Transaction failed");
+            // Deletes the active loan
             delete activeLoans[_lender][msg.sender];
-            // The else block is run if the loan has only one owner
+            // The else block is ran if the loan was never fractionally sold
         } else {
+            /*
+            The loans total debt is assigned in a variable so 
+            that the operation does not have to happen twice. 
+            */
             uint256 totalDebtFull = activeLoans[_lender][msg.sender].amount +
                 activeLoans[_lender][msg.sender].interestAmount;
             require(
                 msg.value == totalDebtFull,
                 "Amount paid back must be exact"
             );
+            // Transfers amount owed to lender from borrower
             (bool success, ) = _lender.call{value: totalDebtFull}("");
             require(success, "Transaction failed");
+            // Deletes the active loan
             delete activeLoans[_lender][msg.sender];
         }
     }
 
-    //Lenders can call this function to sell off their loan to someone else
+    /*
+    Lenders have the right to sell a portion of the loan to a buyer. They can
+    either sell a fraction of the loan, or it's entirety. If the lender sells
+    a percent portion of the loan, they remain the initial lender, and the fractional
+    buyer gets added to the loan. All active loans can only have one fractional owner.
+    If a lender sells 100 percent of the loan, a buyer will become the 
+    new lender, and is free sell any amount of the loan if they choose
+    to do so. 
+    */
     function listLoan(
         address _borrower,
         uint256 _salePrice,
@@ -145,49 +177,55 @@ contract Loans {
             activeLoans[msg.sender][_borrower].isActive,
             "You do not have the rights to sell this loan"
         );
+        // Checks whether the loan has already been listed or not
         require(
             activeLoans[msg.sender][_borrower].fractionalOwner == address(0),
             "Loan can only be sold once"
         );
+        // Sets the loan's for sale price
         activeLoans[msg.sender][_borrower].forSalePrice = _salePrice;
+        // Sets the fractional percentage of loan for sale
         activeLoans[msg.sender][_borrower]
             .loanFractionPercentage = _loanFraction;
+        // Changes loan from being NOT for sale to FOR sale.
         activeLoans[msg.sender][_borrower].isForSale = true;
     }
 
+    // Users can buy full loan from a lender
     function buyLoan(address payable _lender, address _borrower)
         external
         payable
         blackListedCheck
+        correctETHForSalePrice(_lender, _borrower)
+        isForSale(_lender, _borrower)
     {
-        require(activeLoans[_lender][_borrower].isForSale, "non-existant");
-        require(
-            msg.value == activeLoans[_lender][_borrower].forSalePrice,
-            "Incorrect ether amt"
-        );
-        //Calculates the fractional split amount if a lender wants to sell less than 100% of loan
-        // if (activeLoans[_lender][_borrower].loanFractionPercentage == 100) {
-        //Changes ownership by switching the key to loan
+        // A new loan is created with the same fields as the original loan
         activeLoans[msg.sender][_borrower] = activeLoans[_lender][_borrower];
+        // The necessary loan fields are reset to make the loan no longer for sale
         activeLoans[msg.sender][_borrower].loanFractionPercentage = 0;
         activeLoans[msg.sender][_borrower].forSalePrice = 0;
         activeLoans[msg.sender][_borrower].isForSale = false;
+        // The original lenders loan is deleted
         delete activeLoans[_lender][_borrower];
+        // Buyer transfers ETH to the original lender
         (bool success, ) = _lender.call{value: msg.value}("");
         require(success, "Transaction failed");
     }
 
+    // Users can buy portions of loans from lenders if available
     function buyLoanFraction(
         address payable _lender,
         address _borrower,
+        // fractionalLoanAmount and newBaseLoanAmount are determined from front end
         uint256 fractionalLoanAmount,
         uint256 newBaseLoanAmount
-    ) public payable {
-        require(activeLoans[_lender][_borrower].isForSale, "non-existant");
-        require(
-            msg.value == activeLoans[_lender][_borrower].forSalePrice,
-            "Incorrect ether amt"
-        );
+    )
+        external
+        payable
+        blackListedCheck
+        correctETHForSalePrice(_lender, _borrower)
+        isForSale(_lender, _borrower)
+    {
         //Assigns amounts and percentage split of target loan and updates struct fields
         activeLoans[_lender][_borrower]
             .loanFractionAmount = fractionalLoanAmount;
@@ -207,6 +245,10 @@ contract Loans {
         delete proposedLoans[msg.sender];
     }
 
+    /*
+    If an active loan exceeds duration, the lender can blacklist the borrower.
+    This will prevent the borrower from using any function within smart contract.
+    */
     function blacklistAddress(address _borrower) external {
         require(activeLoans[msg.sender][_borrower].isActive, "Loan not active");
         require(
@@ -219,6 +261,7 @@ contract Loans {
         emit Blacklisted(_borrower);
     }
 
+    // Enables anyone to view a one loan proposal at a time.
     function viewLoanProposals(address _borrower)
         public
         view
@@ -227,6 +270,7 @@ contract Loans {
         return proposedLoans[_borrower];
     }
 
+    // Enables anyone to view one active loan at a time.
     function viewActiveLoans(address _lender, address _borrower)
         public
         view
@@ -235,11 +279,29 @@ contract Loans {
         return activeLoans[_lender][_borrower];
     }
 
+    // Checks if msg.sender is blacklisted
     modifier blackListedCheck() {
-        // Checks if the function caller's address is blacklisted
         require(
             isBlacklisted[msg.sender] == false,
             "This address is blacklisted"
+        );
+        _;
+    }
+
+    // Confirms that msg.sender is sending the correct msg.value with transaction
+    modifier correctETHForSalePrice(address _lender, address _borrower) {
+        require(
+            msg.value == activeLoans[_lender][_borrower].forSalePrice,
+            "Incorrect ether amount"
+        );
+        _;
+    }
+
+    // Confirms that the loan msg.sender is looking for is for sale
+    modifier isForSale(address _lender, address _borrower) {
+        require(
+            activeLoans[_lender][_borrower].isForSale,
+            "Active loan is not for sale"
         );
         _;
     }
