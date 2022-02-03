2810d7b60447c37bf3a1bb341ca107c23e73e9b4
McManOfTheLand <forrestchew@gmail.com>
2022-02-03 10:25:37 -0800

    refactored tests to be clearer

--- debt_marketplace/tests/test_integration/test_lending.py
+++ debt_marketplace/tests/test_integration/test_lending.py
@@ -1,3 +1,4 @@
+# Tests the lend function functionality in smart contract
 import brownie
 from brownie import accounts
 from web3 import Web3
@@ -5,20 +6,18 @@ from web3 import Web3
 # using Ganache-cli as the provider
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
 
-
-def test_lend(deploy_contract, propose_loans, lend, account):
+# Tests a basic lend. A user fills a borrowers loan proposal and becomes a lender
+def test_lend(deploy_contract, lend, account):
     borrower = account
     lender = accounts[1]
     contract = deploy_contract
-    # Popose loan from main account in order to lend
-    propose_loans
     lend
-    # collecting borrower's proposed loan struct fields with lender ownership
+    # Confirms that the loan becomes an active loan and that it's struct fields match the proposals
     assert contract.activeLoans(lender, borrower)[0] == w3.toWei(1, "ether")
     assert contract.activeLoans(lender, borrower)[1] == 5
     assert contract.activeLoans(lender, borrower)[2] == w3.toWei(0.05, "ether")
     # Multiplies the input number of days, which in this case is 10,
-    # by 86,400 to get the number of seconds
+    # by 86,400 to get the number of seconds. This will help determine when the loan expires down the road.
     assert contract.activeLoans(lender, borrower)[3] == 10 * 86400
     assert (
         contract.activeLoans(lender, borrower)[4]
@@ -36,23 +35,12 @@ def test_lend(deploy_contract, propose_loans, lend, account):
     assert borrower.balance() == w3.toWei(101, "ether")
     assert lender.balance() == w3.toWei(99, "ether")
 
-
-def test_lend_reverts(deploy_contract, account):
-    borrower = account
-    lender = accounts[1]
-    contract = deploy_contract
-    with brownie.reverts("Account has no active loan proposals"):
-        lend_tx = contract.lend(
-            borrower, {"from": lender, "value": w3.toWei(1, "ether")}
-        )
-        lend_tx.wait(1)
-
-
-def test_loan_proposal_deletion(deploy_contract, propose_loans, lend, account):
+# Confirms that the loan proposal is deleted when it becomes an active loan
+def test_loan_proposal_deletion(deploy_contract, lend, account):
     borrower = account
     contract = deploy_contract
-    propose_loans
     lend
+    # The old loan proposal is zeroed out
     assert contract.proposedLoans(borrower)[0] == 0
     assert contract.proposedLoans(borrower)[1] == 0
     assert contract.proposedLoans(borrower)[2] == 0
@@ -67,3 +55,15 @@ def test_loan_proposal_deletion(deploy_contract, propose_loans, lend, account):
     assert contract.proposedLoans(borrower)[9] is False
     assert contract.proposedLoans(borrower)[10] is False
     assert contract.proposedLoans(borrower)[11] is False
+
+# Confirms that there has to be a loan proposal to lend to it
+def test_lend_reverts(deploy_contract, account):
+    borrower = account
+    lender = accounts[1]
+    contract = deploy_contract
+    with brownie.reverts("Account has no active loan proposals"):
+        lend_tx = contract.lend(
+            borrower, {"from": lender, "value": w3.toWei(1, "ether")}
+        )
+        lend_tx.wait(1)
+
