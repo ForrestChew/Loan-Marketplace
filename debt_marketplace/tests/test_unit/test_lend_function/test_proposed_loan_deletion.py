39ed62ffb2e006fb2445bce537ffefcb48e6a5b4
McManOfTheLand <forrestchew@gmail.com>
2022-02-05 11:25:41 -0800

    Refactored tests to be more concise

--- debt_marketplace/tests/test_unit/test_lend_function/test_proposed_loan_deletion.py
+++ debt_marketplace/tests/test_unit/test_lend_function/test_proposed_loan_deletion.py
@@ -1,79 +1,77 @@
-# Checks that the proposed loan mapping is deleted
+# Confirms that the loan proposal is deleted once someone lends to it. 
+# The proposal becomes an active loan. That's why the proposal is deleted.
 from web3 import Web3
 
 # using Ganache-cli as the provider
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
 
 
-def test_deleted_proposed_amount(deploy_contract, propose_loans, lend, account):
+# Confirms that the loan proposal amount is deleted 
+def test_deleted_proposed_amount(deploy_contract, lend, account):
     contract = deploy_contract
-    propose_loans
     lend
     loan_amount = contract.proposedLoans(account)[0]
     assert loan_amount == 0
 
 
-def test_deleted_proposed_interest_percent(
-    deploy_contract, propose_loans, lend, account
-):
+# Confirms that the loan proposal interest percent is deleted
+def test_deleted_proposed_interest_percent(deploy_contract, lend, account):
     contract = deploy_contract
-    propose_loans
     lend
     interest_rate_percent = contract.proposedLoans(account)[1]
     assert interest_rate_percent == 0
 
 
-def test_deleted_proposed_interest_amount(
-    deploy_contract, propose_loans, lend, account
-):
+# Confirms that the loan proposal interest amount is deleted
+def test_deleted_proposed_interest_amount(deploy_contract, lend, account):
     contract = deploy_contract
-    propose_loans
     lend
     interest_rate_amount = contract.proposedLoans(account)[2]
     assert interest_rate_amount == 0
 
 
-def test_deleted_proposed_loan_duration(deploy_contract, propose_loans, lend, account):
+# Confirms that the loan proposal duration is deleted
+def test_deleted_proposed_loan_duration(deploy_contract, lend, account):
     contract = deploy_contract
-    propose_loans
     lend
     active_loan_duration = contract.proposedLoans(account)[3]
     assert active_loan_duration == 0
 
 
-def test_deleted_proposed_loan_duration(deploy_contract, propose_loans, lend, account):
+# Confirms that the loan proposal timestamp start remains zero
+def test_deleted_proposed_loan_timestamp_start(deploy_contract, lend, account):
     contract = deploy_contract
-    propose_loans
     lend
     start_timestamp = contract.proposedLoans(account)[4]
     assert start_timestamp == 0
 
 
-def test_deleted_proposed_sale_price(deploy_contract, propose_loans, lend, account):
+# Confirms that the loan proposal sale price remains zero
+def test_deleted_proposed_sale_price(deploy_contract, lend, account):
     contract = deploy_contract
-    propose_loans
     lend
     active_loan_sale_price = contract.proposedLoans(account)[5]
     assert active_loan_sale_price == 0
 
 
-def test_deleted_proposed_sale_price(deploy_contract, propose_loans, lend, account):
+# Confirms that the loan proposal's fraction percentage remains zero
+def test_deleted_proposed_fraction_percentage(deploy_contract, lend, account):
     contract = deploy_contract
-    propose_loans
     lend
     loan_fraction_percentage = contract.proposedLoans(account)[6]
     assert loan_fraction_percentage == 0
 
 
-def test_deleted_proposed_sale_price(deploy_contract, propose_loans, lend, account):
+# Confirms that the loan proposal's fraction amount remains zero
+def test_deleted_proposed_fraction_amount(deploy_contract, lend, account):
     contract = deploy_contract
-    propose_loans
     lend
     loan_fraction_amount = contract.proposedLoans(account)[7]
     assert loan_fraction_amount == 0
 
 
-def test_deleted_proposed_sale_price(deploy_contract, propose_loans, lend, account):
+# Confirms that the loan proposal's fractionial owner is the zero address
+def test_deleted_proposed_fractional_owner(deploy_contract, propose_loans, lend, account):
     contract = deploy_contract
     propose_loans
     lend
@@ -81,25 +79,25 @@ def test_deleted_proposed_sale_price(deploy_contract, propose_loans, lend, accou
     assert fractiona_owner == "0x0000000000000000000000000000000000000000"
 
 
-def test_deleted_proposed_sale_price(deploy_contract, propose_loans, lend, account):
+# Confirms that the loan proposal is NOT proposed
+def test_deleted_proposed_is_proposed(deploy_contract, lend, account):
     contract = deploy_contract
-    propose_loans
     lend
     is_proposed = contract.proposedLoans(account)[9]
     assert is_proposed == False
 
 
-def test_deleted_proposed_sale_price(deploy_contract, propose_loans, lend, account):
+# Confirms that the loan proposal is NOT active
+def test_deleted_proposed_is_active(deploy_contract, lend, account):
     contract = deploy_contract
-    propose_loans
     lend
     is_active = contract.proposedLoans(account)[10]
     assert is_active == False
 
 
-def test_deleted_proposed_sale_price(deploy_contract, propose_loans, lend, account):
+# Confirms that the loan proposal is NOT for sale
+def test_deleted_proposed_sale_price(deploy_contract, lend, account):
     contract = deploy_contract
-    propose_loans
     lend
     is_for_sale = contract.proposedLoans(account)[11]
     assert is_for_sale == False
