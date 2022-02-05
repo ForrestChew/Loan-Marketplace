033307274548199f47250b80ae70028127e211b7
McManOfTheLand <forrestchew@gmail.com>
2022-02-05 11:27:12 -0800

    Better comment

--- debt_marketplace/tests/test_unit/test_lend_function/test_requirement_statements.py
+++ debt_marketplace/tests/test_unit/test_lend_function/test_requirement_statements.py
@@ -6,7 +6,8 @@ from web3 import Web3
 # using Ganache-cli as the provider
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
 
-# Confirms that lending to an account without a proposal cannot be done
+
+# Confirms that lending to an account without a proposal CANNOT be done
 def test_lend_revert(deploy_contract, account):
     contract = deploy_contract
     borrower = account
