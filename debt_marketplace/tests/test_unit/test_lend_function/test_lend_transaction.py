6e7ba5fdf1d146477a70c36f85850ba7970dbf79
McManOfTheLand <forrestchew@gmail.com>
2022-02-05 11:14:39 -0800

    added comments. Also removed unnecessary fixture

--- debt_marketplace/tests/test_unit/test_lend_function/test_lend_transaction.py
+++ debt_marketplace/tests/test_unit/test_lend_function/test_lend_transaction.py
@@ -7,19 +7,19 @@ from web3 import Web3
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
 
 
-def test_eth_sent(deploy_contract, propose_loans, lend):
+#Lender's balance is correct
+def test_eth_sent(deploy_contract, lend):
     lender = accounts[1]
     deploy_contract
-    propose_loans
     lend
     # Confirms an decrease of one ETH to the lender ETH balance
     assert lender.balance() == w3.toWei(99, "ether")
 
 
-def test_eth_recieved(deploy_contract, propose_loans, lend, account):
+#Borrower's balance is correct
+def test_eth_recieved(deploy_contract, lend, account):
     borrower = account
     deploy_contract
-    propose_loans
     lend
     # Confirms an increase of one ETH to the borrowers ETH balance
     assert borrower.balance() == w3.toWei(101, "ether")
