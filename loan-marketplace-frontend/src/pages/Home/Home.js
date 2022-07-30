0832d7bd74c80e7852a0c4154157dc455ff7ea68
Forrest Chew <forrestchew@gmail.com>
2022-07-30 14:22:16 -0700

    Initial Structure and layout

--- loan-marketplace-frontend/src/pages/Home/Home.js
+++ loan-marketplace-frontend/src/pages/Home/Home.js
@@ -1,7 +1,41 @@
 import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <section className="title">
+        <h1>
+          Loan Marketplace <em>(not predetory)</em>
+        </h1>
+        <h4>Borrow, Lend, and sell debt through this defi protocal.</h4>
+      </section>
+      <section className="stats">
+        <h1>Active</h1>
+        <ul className="stat-items">
+          <div className="stat-contents">
+            <li>Lenders</li>
+            <li>{0}</li>
+          </div>
+          <div className="stat-contents">
+            <li>Borrowers</li>
+            <li>{0}</li>
+          </div>
+          <div className="stat-contents">
+            <li>Proposals</li>
+            <li>{0}</li>
+          </div>
+          <div className="stat-contents">
+            <li>Loans</li>
+            <li>{0}</li>
+          </div>
+          <div className="stat-contents">
+            <li>Fractional Loans</li>
+            <li>{0}</li>
+          </div>
+        </ul>
+      </section>
+    </>
+  );
 };
 
 export default Home;
