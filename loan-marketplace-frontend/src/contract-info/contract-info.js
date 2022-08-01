e788f4fddb881708e53cae1084058c3ff9ee628b
Forrest Chew <forrestchew@gmail.com>
2022-07-31 20:42:39 -0700

    Contract info and interactions

---
+++ loan-marketplace-frontend/src/contract-info/contract-info.js
@@ -0,0 +1,478 @@
+export const contractAddress = "0x087923b081849D2852E60D8417C5b272598172Ab";
+
+export const contractABI = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_owner",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "_listingFee",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "totalDebt",
+        type: "uint256",
+      },
+    ],
+    name: "DebtPaid",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "address",
+        name: "lender",
+        type: "address",
+      },
+    ],
+    name: "Lend",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "loanId",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "address",
+        name: "buyer",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "percentage",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "price",
+        type: "uint256",
+      },
+    ],
+    name: "LoanFractionSold",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "loanAmount",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "interestRate",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "loanDuration",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "address",
+        name: "borrower",
+        type: "address",
+      },
+    ],
+    name: "LoanProposalCreated",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "loanId",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "percentage",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "price",
+        type: "uint256",
+      },
+    ],
+    name: "NewLoanFraction",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_seller",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "_loanId",
+        type: "uint256",
+      },
+    ],
+    name: "buyLoanFraction",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "fractionalSales",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "loanId",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "price",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "percentage",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getLoanProposals",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "uint256",
+            name: "idx",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "id",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "loanAmount",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "interestRate",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "loanDuration",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "loanStartTime",
+            type: "uint256",
+          },
+          {
+            internalType: "address",
+            name: "borrower",
+            type: "address",
+          },
+          {
+            internalType: "address[]",
+            name: "lenders",
+            type: "address[]",
+          },
+          {
+            internalType: "bool",
+            name: "isProposed",
+            type: "bool",
+          },
+          {
+            internalType: "bool",
+            name: "isActive",
+            type: "bool",
+          },
+          {
+            internalType: "bool",
+            name: "isForSale",
+            type: "bool",
+          },
+        ],
+        internalType: "struct LoanFactory.Loan[]",
+        name: "",
+        type: "tuple[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_loanId",
+        type: "uint256",
+      },
+    ],
+    name: "getTotalDebt",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_proposalId",
+        type: "uint256",
+      },
+    ],
+    name: "lend",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "lenderPercentAmounts",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "listingFee",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "loanIdToLoan",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "idx",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "loanAmount",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "interestRate",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "loanDuration",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "loanStartTime",
+        type: "uint256",
+      },
+      {
+        internalType: "address",
+        name: "borrower",
+        type: "address",
+      },
+      {
+        internalType: "bool",
+        name: "isProposed",
+        type: "bool",
+      },
+      {
+        internalType: "bool",
+        name: "isActive",
+        type: "bool",
+      },
+      {
+        internalType: "bool",
+        name: "isForSale",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_loanId",
+        type: "uint256",
+      },
+    ],
+    name: "payoffDebt",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_loanAmount",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_interestRate",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_loanDuration",
+        type: "uint256",
+      },
+    ],
+    name: "proposeLoan",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_loanId",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_price",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_percentage",
+        type: "uint256",
+      },
+    ],
+    name: "sellLoanFraction",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
---
+++ loan-marketplace-frontend/src/contract-info/contract-interactions.js
@@ -0,0 +1,47 @@
+import { ethers } from "ethers";
+import { contractAddress, contractABI } from "./contract-info";
+
+const getProvider = () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  return provider;
+};
+
+const getSigner = async () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  return signer;
+};
+
+// Pass in a signer to get write access to the smart contract.
+// Pass in a provider to get read only access to the smart contract.
+const getContractInstance = (providerOrSigner) => {
+  const contractInstance = new ethers.Contract(
+    contractAddress,
+    contractABI,
+    providerOrSigner
+  );
+  return contractInstance;
+};
+
+const invokeProposeLoan = async (loanAmount, interestRate, duration) => {
+  const listingFee = ethers.utils.parseEther("1");
+  const signer = await getSigner();
+  const contract = getContractInstance(signer);
+  await contract.proposeLoan(loanAmount, interestRate, duration, {
+    value: listingFee,
+  });
+};
+
+const invokeLend = async (proposalId, loanAmount) => {
+  const signer = await getSigner();
+  const contract = getContractInstance(signer);
+  await contract.lend(proposalId, { value: loanAmount });
+};
+
+const callgetLoanProposals = async () => {
+  const provider = getProvider();
+  const contract = getContractInstance(provider);
+  return await contract.getLoanProposals();
+};
+
+export { invokeProposeLoan, invokeLend, callgetLoanProposals };
