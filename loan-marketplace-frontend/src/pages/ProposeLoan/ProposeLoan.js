6c4c79d475552fe5cba69bbdc0bfe7ba4dd98e43
Forrest Chew <forrestchew@gmail.com>
2022-07-31 20:43:49 -0700

    loan strip initial structure

--- loan-marketplace-frontend/src/pages/ProposeLoan/ProposeLoan.js
+++ loan-marketplace-frontend/src/pages/ProposeLoan/ProposeLoan.js
@@ -1,35 +1,67 @@
+import { useState } from "react";
+import { ethers } from "ethers";
+import { invokeProposeLoan } from "../../contract-info/contract-interactions";
 import "./ProposeLoan.css";
 
 const ProposeLoan = () => {
+  const [loanProposal, setLoanProposal] = useState({
+    amount: 0,
+    rate: 0,
+    duration: 0,
+  });
+
+  const submitForm = async (e) => {
+    e.preventDefault();
+    const loanAmountToWei = ethers.utils.parseEther(loanProposal.amount);
+    await invokeProposeLoan(
+      loanAmountToWei,
+      loanProposal.rate,
+      loanProposal.duration
+    );
+  };
+
+  const handleInputChange = (e) => {
+    e.preventDefault();
+    const name = e.target.name;
+    const value = e.target.value;
+    setLoanProposal({ ...loanProposal, [name]: value });
+  };
+
   return (
     <section className="form-container">
-      <form className="propose-loan-form">
+      <form className="propose-loan-form" onSubmit={submitForm}>
         <h2>Propose Loan</h2>
         <label>Loan Amount</label>
         <input
           type="number"
           name="amount"
+          value={loanProposal.amount}
           placeholder="To Borrow"
           min="0"
           required
+          onChange={handleInputChange}
         ></input>
         <label>Interest Rate</label>
         <input
           type="number"
-          name="iRate"
+          name="rate"
+          value={loanProposal.rate}
           placeholder="Interest Rate Percent"
           min="0"
           required
+          onChange={handleInputChange}
         ></input>
         <label>Duration</label>
         <input
           type="number"
           name="duration"
+          value={loanProposal.duration}
           placeholder="Loan Duration"
           min="0"
           required
+          onChange={handleInputChange}
         ></input>
-        <button>Propose</button>
+        <button className="propose-btn">Propose</button>
       </form>
     </section>
   );
