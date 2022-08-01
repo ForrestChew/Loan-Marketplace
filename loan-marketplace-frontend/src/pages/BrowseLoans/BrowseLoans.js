4ab2607a8b2891c1cd9318e7eb1934c9ceca5ecf
Forrest Chew <forrestchew@gmail.com>
2022-07-31 20:43:13 -0700

    BrowseLoans page - initial structure

--- loan-marketplace-frontend/src/pages/BrowseLoans/BrowseLoans.js
+++ loan-marketplace-frontend/src/pages/BrowseLoans/BrowseLoans.js
@@ -1,7 +1,35 @@
+import { useState, useEffect } from "react";
+import { callgetLoanProposals } from "../../contract-info/contract-interactions";
+import LoanProposal from "../../components/LoanProposal/LoanProposal";
 import "./BrowseLoans.css";
 
 const BrowseLoans = () => {
-  return <div>BrowseLoans</div>;
+  const [loanProposals, setLoanProposals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getLoanProposals = async () => {
+      const proposals = await callgetLoanProposals();
+      proposals.map((proposal) => {
+        setLoanProposals((loanProposals) => [...loanProposals, proposal]);
+      });
+    };
+    getLoanProposals();
+    setIsLoading(false);
+  }, []);
+
+  return (
+    <section className="proposals">
+      {isLoading ? (
+        <h1>LOADING...</h1>
+      ) : (
+        loanProposals.map((proposalAttributes, idx) => {
+          if (proposalAttributes.lenders.length == 0)
+            return <LoanProposal key={idx} attributes={proposalAttributes} />;
+        })
+      )}
+    </section>
+  );
 };
 
 export default BrowseLoans;
